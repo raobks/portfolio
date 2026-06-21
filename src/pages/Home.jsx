e0d11967426c9cb11f136026f726981bf31ae41d
Bhupender Singh <bhupenderrao28@gmail.com>
2026-06-21 23:40:56 +0530

    add skills and projects section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { StarBackground } from "@/components/StarBackground";
 import { Navbar } from "@/components/Navbar";
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
 export const Home = () => {
   return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
@@ -17,6 +19,8 @@ export const Home = () => {
     <main>
       <HeroSection />
       <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
     </main>
 
     {/* Footer */}
